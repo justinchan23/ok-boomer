167ca6f9e298b05c2084e4d1cd500ecf5ab9f94a
Gabi Geollegue <gabigeollegue@gmail.com>
2019-11-21 20:14:54 +0000

    init express setup

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -1,11 +1,30 @@
+// load .env data into process.env
+const ENV = process.env.ENV || "development";
+
+if (ENV === "development") {
+  require("dotenv").config();
+}
+
+// Web server config
+const PORT = process.env.PORT || 8080;
 const Express = require("express");
-const App = Express();
 const BodyParser = require("body-parser");
-const PORT = 8080;
+// const sass = require("node-sass-middleware");
+const App = Express();
+const morgan = require("morgan");
+// const cookieSession = require("cookie-session");
+
+// PG database client/connection setup
+const { Pool } = require("pg");
+
+const dbParams = require("./lib/db.js");
+const db = new Pool(dbParams);
+db.connect();
 
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(Express.static("public"));
+App.use(morgan("dev"));
 
 // Sample GET route
 App.get("/api/data", (req, res) =>
