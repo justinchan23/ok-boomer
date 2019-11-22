222af2407fa38e429b68881f4e6624c7be2689d9
Jacky Lau <jackylau0126@gmail.com>
2019-11-22 04:23:24 +0000

    implement a sample image to move inside the game browser

--- express/js/game.js
+++ express/js/game.js
@@ -1,8 +1,8 @@
 /* eslint-disable func-style */
 const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  type: Phaser.CANVAS,
+  width: 1280,
+  height: 720,
   scene: {
     preload: preload,
     create: create,
@@ -12,12 +12,30 @@ const config = {
 
 const game = new Phaser.Game(config);
 
+let dog;
+
 function preload() {
   this.load.image("doggy", "assets/doggy.png");
 }
 
 function create() {
-  this.add.image(400, 300, "doggy");
+  dog = this.add.image(400, 300, "doggy");
+
+  this.input.keyboard.on("keydown-A", () => {
+    dog.x -= 4;
+  });
+
+  this.input.keyboard.on("keydown-D", () => {
+    dog.x += 4;
+  });
+
+  this.input.keyboard.on("keydown-W", () => {
+    dog.y -= 4;
+  });
+
+  this.input.keyboard.on("keydown-S", () => {
+    dog.y += 4;
+  });
 }
 
 function update() {}
