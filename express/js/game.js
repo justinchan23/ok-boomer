93bd7694dbec7b63d25319e161abc85aba376a1f
Darrenni97 <darrenni_@hotmail.com>
2019-11-22 15:33:22 -0800

    creating game map & refactored player movement

--- express/js/game.js
+++ express/js/game.js
@@ -1,41 +1,57 @@
 /* eslint-disable func-style */
-const config = {
-  type: Phaser.CANVAS,
-  width: 1280,
-  height: 720,
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-  }
-};
-
-const game = new Phaser.Game(config);
 
 let dog;
 
-function preload() {
-  this.load.image("doggy", "assets/doggy.png");
+class StartScene extends Phaser.Scene {
+  preload() {
+    this.load.image("doggy", "assets/doggy.png");
+
+    this.load.image("blocks", "assets/maps/blocks.png");
+    this.load.image("floor", "assets/maps/floor.png");
+    this.load.tilemapTiledJSON("map1", "assets/maps/map1.json");
+  }
+
+  create() {
+    this.map = this.make.tilemap({ key: "map1" });
+    //this.map = this.add.tilemap("map1");
+    const tileset = this.map.addTilesetImage("blocks", "blocks");
+    const tileset2 = this.map.addTilesetImage("floor", "floor");
+
+    this.blocksLayer = this.map.createStaticLayer("blocks", tileset);
+    this.floorLayer = this.map.createStaticLayer("floor", tileset2);
+
+    //character loading
+    this.player = this.physics.add.image(640, 640, "doggy");
+    //character movment
+    this.input.keyboard.on("keydown-A", () => {
+      this.player.body.setVelocityX(-400);
+    });
+
+    this.input.keyboard.on("keydown-D", () => {
+      this.player.body.setVelocityX(400);
+    });
+
+    this.input.keyboard.on("keydown-W", () => {
+      this.player.body.setVelocityY(-400);
+    });
+
+    this.input.keyboard.on("keydown-S", () => {
+      this.player.body.setVelocityY(400);
+    });
+  }
+
+  update() {}
 }
 
-function create() {
-  dog = this.add.image(400, 300, "doggy");
+const config = {
+  type: Phaser.CANVAS,
+  width: 1024,
+  height: 1024,
+  physics: {
+    arcade: {},
+    default: "arcade"
+  },
+  scene: [StartScene]
+};
 
-  this.input.keyboard.on("keydown-A", () => {
-    dog.x -= 4;
-  });
-
-  this.input.keyboard.on("keydown-D", () => {
-    dog.x += 4;
-  });
-
-  this.input.keyboard.on("keydown-W", () => {
-    dog.y -= 4;
-  });
-
-  this.input.keyboard.on("keydown-S", () => {
-    dog.y += 4;
-  });
-}
-
-function update() {}
+const game = new Phaser.Game(config);
