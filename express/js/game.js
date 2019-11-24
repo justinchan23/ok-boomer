8c1480050ba0934499c7a3517c7827758158dee6
Darrenni97 <darrenni_@hotmail.com>
2019-11-23 17:46:56 -0800

    block layer collision & velocity movement

--- express/js/game.js
+++ express/js/game.js
@@ -1,60 +1,72 @@
 /* eslint-disable func-style */
+const config = {
+  type: Phaser.AUTO,
+  width: 1500,
+  height: 1024,
+  physics: {
+    default: "arcade",
+    arcade: { debug: false }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+const game = new Phaser.Game(config);
+
 let up;
 let left;
 let right;
 let down;
-let dog;
 
-class StartScene extends Phaser.Scene {
-  preload() {
-    this.load.image("white", "assets/characters/white.png");
+function preload() {
+  this.load.image("white", "assets/characters/white.png");
 
-    this.load.image("blocks", "assets/maps/blocks.png");
-    this.load.image("floor", "assets/maps/floor.png");
-    this.load.tilemapTiledJSON("map1", "assets/maps/map1.json");
-  }
-
-  create() {
-    this.map = this.make.tilemap({ key: "map1" });
-    //this.map = this.add.tilemap("map1");
-    const tileset = this.map.addTilesetImage("blocks", "blocks");
-    const tileset2 = this.map.addTilesetImage("floor", "floor");
-
-    this.blocksLayer = this.map.createStaticLayer("blocks", tileset);
-    this.floorLayer = this.map.createStaticLayer("floor", tileset2);
-
-    //character loading
-    this.player = this.physics.add.image(640, 640, "white");
-
-    up = this.input.keyboard.addKey("W");
-    left = this.input.keyboard.addKey("A");
-    right = this.input.keyboard.addKey("D");
-    down = this.input.keyboard.addKey("S");
-  }
-
-  update() {
-    //movement for characters
-    if (this.input.keyboard.checkDown(up, 0)) {
-      this.player.y -= 4;
-    } else if (this.input.keyboard.checkDown(left, 0)) {
-      this.player.x -= 4;
-    } else if (this.input.keyboard.checkDown(right, 0)) {
-      this.player.x += 4;
-    } else if (this.input.keyboard.checkDown(down, 0)) {
-      this.player.y += 4;
-    }
-  }
+  this.load.tilemapTiledJSON("map1", "assets/maps/map1.json");
+  this.load.image("floor", "assets/maps/floor.png");
+  this.load.image("blocks", "assets/maps/blocks.png");
 }
 
-const config = {
-  type: Phaser.CANVAS,
-  width: 1024,
-  height: 1024,
-  physics: {
-    arcade: {},
-    default: "arcade"
-  },
-  scene: [StartScene]
-};
+function create() {
+  this.map = this.add.tilemap("map1");
 
-const game = new Phaser.Game(config);
+  let tileset = this.map.addTilesetImage("blocks", "blocks");
+  let tileset2 = this.map.addTilesetImage("floor", "floor");
+
+  this.floorLayer = this.map.createStaticLayer("floor", [tileset2], 0, 0);
+  this.blocksLayer = this.map.createStaticLayer("blocks", [tileset], 0, 0);
+
+  this.player = this.physics.add.sprite(96, 96, "white");
+  //collision for world bounds
+  this.player.setCollideWorldBounds(true);
+
+  this.blocksLayer.setCollisionByProperty({ collides: true });
+  this.physics.add.collider(this.player, this.blocksLayer);
+
+  up = this.input.keyboard.addKey("W");
+  left = this.input.keyboard.addKey("A");
+  right = this.input.keyboard.addKey("D");
+  down = this.input.keyboard.addKey("S");
+}
+
+const speed = 200;
+function update() {
+  // Stop any previous movement from the last frame
+  this.player.body.setVelocity(0);
+  // Horizontal movement
+  if (this.input.keyboard.checkDown(left, 0)) {
+    this.player.body.setVelocityX(-200);
+  } else if (this.input.keyboard.checkDown(right, 0)) {
+    this.player.body.setVelocityX(200);
+  }
+  // Vertical movement
+  if (this.input.keyboard.checkDown(up, 0)) {
+    this.player.body.setVelocityY(-200);
+  } else if (this.input.keyboard.checkDown(down, 0)) {
+    this.player.body.setVelocityY(200);
+  }
+  // Normalize and scale the velocity so that player can't move faster along a diagonal
+  this.player.body.velocity.normalize().scale(speed);
+}
