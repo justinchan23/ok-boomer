2b56b74a2399f262183fd5ace9211b2c345fc92f
Gabi Geollegue <gabigeollegue@gmail.com>
2019-11-25 04:50:42 +0000

    style buttons on react side

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
-import Dpad from "./Dpad";
 import Button from "./Button";
 import "./App.css";
+import io from "socket.io-client";
+import "./Dpad.css";
+let socketClient = io("http://192.168.88.61:3001/players");
 
 class App extends Component {
   constructor() {
@@ -13,10 +15,79 @@ class App extends Component {
     return (
       <div>
         <div class="bombDiv">
-          <Button bomb>Bomb</Button>
+          <Button bomb></Button>
         </div>
         <div class="dpad">
-          <Dpad></Dpad>
+          <div class="UpOnly">
+            <button
+              class="buttonArrow"
+              id="right"
+              onTouchStart={() => {
+                socketClient.emit("playerMovement", {
+                  playerID: socketClient.id,
+                  move: "Right"
+                });
+              }}
+              onTouchEnd={() => {
+                socketClient.emit("playerMovementEnd", {
+                  playerID: socketClient.id,
+                  move: "Right"
+                });
+              }}
+            ></button>
+          </div>
+          <div class="upDown">
+            <button
+              class="buttonArrow"
+              id="up"
+              onTouchStart={() => {
+                socketClient.emit("playerMovement", {
+                  playerID: socketClient.id,
+                  move: "Up"
+                });
+              }}
+              onTouchEnd={() => {
+                socketClient.emit("playerMovementEnd", {
+                  playerID: socketClient.id,
+                  move: "Up"
+                });
+              }}
+            ></button>
+            <button
+              class="buttonArrow"
+              id="down"
+              onTouchStart={() => {
+                socketClient.emit("playerMovement", {
+                  playerID: socketClient.id,
+                  move: "Down"
+                });
+              }}
+              onTouchEnd={() => {
+                socketClient.emit("playerMovementEnd", {
+                  playerID: socketClient.id,
+                  move: "Down"
+                });
+              }}
+            ></button>
+          </div>
+          <div class="downOnly">
+            <button
+              class="buttonArrow"
+              id="left"
+              onTouchStart={() => {
+                socketClient.emit("playerMovement", {
+                  playerID: socketClient.id,
+                  move: "Left"
+                });
+              }}
+              onTouchEnd={() => {
+                socketClient.emit("playerMovementEnd", {
+                  playerID: socketClient.id,
+                  move: "Left"
+                });
+              }}
+            ></button>
+          </div>
         </div>
       </div>
     );
--- react-front-end/src/Dpad.js
+++ react-front-end/src/Dpad.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
 import Button from "./Button";
-import io from "socket.io-client";
-import "./Dpad.css";
-let socketClient = io("http://192.168.88.61:3001/players");
 
 class Dpad extends Component {
   constructor() {
@@ -21,70 +18,7 @@ class Dpad extends Component {
     //socketClient.emit("functionName, {my: 'data'});
 
     //send socketClient.id back when new player joins/react renders
-    return (
-      <div style={{ textAlign: "center" }}>
-        <Button
-          left
-          onTouchStart={() => {
-            socketClient.emit("playerMovement", {
-              playerID: socketClient.id,
-              move: "Left"
-            });
-          }}
-          onTouchEnd={() => {
-            socketClient.emit("playerMovementEnd", {
-              playerID: socketClient.id,
-              move: "Left"
-            });
-          }}
-        ></Button>
-        <Button
-          right
-          onTouchStart={() => {
-            socketClient.emit("playerMovement", {
-              playerID: socketClient.id,
-              move: "Right"
-            });
-          }}
-          onTouchEnd={() => {
-            socketClient.emit("playerMovementEnd", {
-              playerID: socketClient.id,
-              move: "Right"
-            });
-          }}
-        ></Button>
-        <Button
-          up
-          onTouchStart={() => {
-            socketClient.emit("playerMovement", {
-              playerID: socketClient.id,
-              move: "Up"
-            });
-          }}
-          onTouchEnd={() => {
-            socketClient.emit("playerMovementEnd", {
-              playerID: socketClient.id,
-              move: "Up"
-            });
-          }}
-        ></Button>
-        <Button
-          down
-          onTouchStart={() => {
-            socketClient.emit("playerMovement", {
-              playerID: socketClient.id,
-              move: "Down"
-            });
-          }}
-          onTouchEnd={() => {
-            socketClient.emit("playerMovementEnd", {
-              playerID: socketClient.id,
-              move: "Down"
-            });
-          }}
-        ></Button>
-      </div>
-    );
+    return <div style={{ textAlign: "center" }}></div>;
   }
 }
 export default Dpad;
